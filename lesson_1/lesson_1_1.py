ea768936de810029dc97e221f5fa24fec5261ddf
elimaakh <elima.akh@mail.ru>
2025-08-31 02:44:52 +0300

    lesson 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,31 @@
-print("привет мир")
+# a = 7
+# b = 8.34
+# str_1 = "легко в ИТ"
+# # b = a
+# # a =
+# # print (b)
+#
+# # a = b = c = 0
+# # a, b, c = 0, 0, 0
+# #
+# # a, b = 8, 10
+# # a, b = b, a
+# # print (a)
+# # print (b)
+#
+# print (type(a))
+# print (type (b))
+# print (type(str_1))
+
+# message = "привет Даниил"
+# count = 0
+# Count = 0
+
+# a = c = 4
+# b, d = "какая нибудь строка", "вторая строка"
+# print (id(a))
+# print (id(c))
+# print (id(b))
+# print (id(d))
+
+
