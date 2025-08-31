77f9a80be5a9dfded91d33e5084cfe331a8c0aa0
elimaakh <elima.akh@mail.ru>
2025-08-31 03:43:53 +0300

    homework 1

---
+++ homework_1_1.py
@@ -0,0 +1,74 @@
+# задание 1
+# name = "Элима"
+# age = 21
+# height = 1.72
+# print (name)
+# print (age)
+# print (height)
+
+# задание 2
+# x = 10
+# print (type(x))
+# x = 25.5
+# print (type(x))
+# x = "Python"
+# print (type(x))
+
+# задание 3
+# a = 7
+# b = a
+# print (a)
+# print (b)
+# a = 10
+# print (b)
+# значение b не изменилось тк переменная b ссылается на первое заданное значение
+
+# задание 4
+# x = y = z = 100
+# print (x, y, z)
+# print (id(x))
+# print (id(y))
+# print (id(z))
+# x, y, z = 1, 2, 3
+# print (x, y, z)
+# print (id(x))
+# print (id(y))
+# print (id(z))
+# id разные
+
+# задание 5
+# a = 5
+# b = 10
+# a, b = b, a
+# print (a, b)
+
+# задание 6
+# не получается создать переменные с названиями "True, print, if" - выдает ошибку потому что это одни из ключевых слов
+# import keyword
+# print(keyword.kwlist)
+
+# задание 7
+# var1 = 42
+# var2 = 3.14
+# var3 = "Hello"
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# var1 = "Компьютер"
+# print(type(var1))
+
+ # задание 8
+# lock = 27
+# bike = 17.5
+# city = "Moscow"
+# country = "Russia"
+# Country = "Brazil"
+# print(lock, bike, city, country, Country)
+# print(type(lock))
+# print(type(bike))
+# print(type(city))
+# print(type(country))
+# print(type(Country))
+# переменная = 10
+# print (переменная)
+# да, переменная с русским именем работает
