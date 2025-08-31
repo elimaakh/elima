6fe9bc0e80edbf92c6b372f5c8b99724cae756c9
elimaakh <elima.akh@mail.ru>
2025-09-01 01:08:57 +0300

    homework 2

---
+++ lesson_2/homework_2.py
@@ -0,0 +1,105 @@
+# задание 1
+# a = 4
+# b = 8
+# x = 3.1
+# y = -4.2
+# print(a, b, x, y)
+# print(type(a))
+# print(type(b))
+# print(type(x))
+# print(type(y))
+from ctypes import pythonapi
+
+# задание 2
+# num1 = 3
+# num2 = 1
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+# задание 3
+# a = 10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# a = -10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# a = 10
+# b = -3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# задание 4
+# print(5 + 3 * 2)
+# print((5 + 3) * 2)
+# print(10 / 2 ** 2)
+
+# задание 5
+# count = 10
+# count += 5
+# count -= 3
+# count *= 2
+# count /= 4
+# print(count)
+
+# строки в python
+# задание 1
+# s1 = "Python"
+# s2 = 'Программирование'
+# print(s1, s2)
+# s3 = """день
+# знаний"""
+# print(s3)
+# empty = ""
+# print(empty)
+
+# задание 2
+# first_name = "Иван"
+# last_name = "Петров"
+# full_name = (first_name + " " + last_name)
+# print(full_name)
+
+# задание 3
+# s = "Возраст: "
+# age = 25
+# Age = s = str(age)
+# print(Age)
+
+# задание 4
+# print("ха " * 5)
+# print("xa " * 2.5) - не получается вывести, выдает ошибку тк нельзя умножить str на float
+
+# задание 5
+# text = "Привет, мир!"
+# print(len(text))
+# str = ""
+# print(len(str))
+
+# задание 6
+# sentence = "Я изучаю Python"
+# print("Python" in sentence)
+# print("Java" in sentence)
+
+# задание 7
+# a = "apple"
+# b = "banana"
+# print(a == b)
+# print(a != b)
+# print(a < b)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
+
+# задание 8
+# print(ord('А'))
+# print(ord('а'))
+# print(ord('я'))
